6af52a618ced43d0678763f90eaf23e658c55a2e
inon19-meet <inon19@meet.mit.edu>
2017-07-31 18:34:15 +0300

    Stopping here

--- turtleSUPERextraFUN.py
+++ turtleSUPERextraFUN.py
@@ -21,28 +21,73 @@ Right = 3
 
 direction = Up
 
-#go Up
-def up():    
+                            #***************go Up
+def Up():    
     global direction
     direction = Up
+    old_pos = turtle.pos()
+    x = old_pos[0]
+    y = old_pos[1]
+    turtle.goto(x,y+10)
+    print(turtle.pos))
     print("you pressed Up")
 
-#go Left
+                            #***************go Left
 def Left():
     global direction
     direction = Left
+    x = old_pos[1]
+    y = old_pos[2]
+    turtle.goto(x-10,y)
+    print(turtle.pos))
     print("you pressed Left")
     
-#go Down
+                            #***************go Down
 def Down():
     global direction
     direction = Down
+    x = old_pos[2]
+    y = old_pos[3]
+    turtle.goto(x,y-10)
+    print(turtle.pos))
     print("you pressed Down")
-#go Right
+
+                             #**************go Right
 def Right():
     global direction
     direction = Right
+    x = old_pos[3]
+    y = old_pos[4]
+    turtle.goto(x+10,y)
+    print(turtle.pos))
     print("you pressed Right")
+
+                             #***************onkeypress
+    #Up***********************
+turtle.onkeypress(Up,UP_ARROW)
+    #Left******************************
+turtle.onkeypress(Left,LEFT_ARROW)
+    #Down************************
+turtle.onkeypress(Down,DOWN_ARROW)
+    #Right***************************
+turtle.onkeypress(Right,RIGHT_ARROW)
+    #tuertle.listen**********************&&!!!!!!!!!!!!!!!11
+turtle.listen()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
